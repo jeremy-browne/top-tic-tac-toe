9e8804ee7e739cbc031bae6002f72f4dbe295550
Jeremy Browne <jeremybrowne1991@hotmail.com>
2021-09-20 10:20:17 +1000

    Added easy AI, fixed renaming bug

--- scripts.js
+++ scripts.js
@@ -1,159 +1,232 @@
 const gameBoard = (() => {
-    const gameArr = new Array(9);
-    const gameElem = "gameBoard";
-    const winningCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-    return {gameArr, gameElem, winningCombos};
+	const gameArr = new Array(9);
+	const gameElem = "gameBoard";
+	const winningCombos = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+		];
+	return { gameArr, gameElem, winningCombos };
 })();
 
-const player = (name, marker) => {
-    this.name = name;
-    this.score = 0;
-    this.marker = marker;
-    return {name, score, marker};
-}
+const player = (name, marker, id) => {
+	this.id = id;
+	this.name = name;
+	this.score = 0;
+	this.marker = marker;
+	return { name, score, marker, id };
+};
 
 const updateScores = () => {
-    const player1Label = document.getElementById("player1Name");
-    const player1Score = document.getElementById("player1Score");
-    player1Label.innerText = player1.name;
-    player1Score.innerHTML = player1.score;
-    
-    const player2Label = document.getElementById("player2Name");
-    const player2Score = document.getElementById("player2Score");
-    player2Label.innerText = player2.name;
-    player2Score.innerHTML = player2.score;
-    activePlayer = player2;
-}
+	const player1Label = document.getElementById("player1Name");
+	const player1Score = document.getElementById("player1Score");
+	player1Label.innerText = player1.name;
+	player1Score.innerText = player1.score;
+
+	const player2Label = document.getElementById("player2Name");
+	const player2Score = document.getElementById("player2Score");
+	player2Label.innerText = player2.name;
+	player2Score.innerText = player2.score;
+};
 
 const gameHandler = () => {
-    winnerName.innerText = "Game in progress...";
-    const player1Card = document.getElementById("player1");
-    const player2Card = document.getElementById("player2");
-    
-    
-    gameBoard.winningCombos.forEach(combo => {
-        let winArr = [];
-        for (let i = 0; i < combo.length; i++) {
-            winArr.push(gameBoard.gameArr[combo[i]]);
-        }
+	winnerName.innerText = "Game in progress...";
+	// Check for win
+	gameBoard.winningCombos.forEach((combo) => {
+		let winArr = [];
+		for (let i = 0; i < combo.length; i++) {
+			winArr.push(gameBoard.gameArr[combo[i]]);
+		}
 
-        if (!winArr.includes(undefined) && winArr.every((item) => item == winArr[0])) {
-            winnerName.innerText = activePlayer.name + " wins!";
-            activePlayer.score += 1;
-            console.table(activePlayer);
-            modal.style.display = "block";
-            updateScores();
-        }
+		if (!winArr.includes(undefined) && winArr.every((item) => item == winArr[0])) {
+			gameOver = true;
+			winnerName.innerText = activePlayer.name + " wins!";
+			activePlayer.score += 1;
+			modal.style.display = "block";
+			updateScores();
+		}
 
-        if (!winArr.includes(undefined) && !winArr.every((item) => item == winArr[0]) && !gameBoard.gameArr.includes(undefined)) {
-            winnerName.innerText = "Nobody Wins!"
-        }
-        console.log(winArr);
-    });
+		if (!winArr.includes(undefined) && !winArr.every((item) => item == winArr[0]) && !gameBoard.gameArr.includes(undefined)) {
+			gameOver = true;
+			winnerName.innerText = "Nobody wins!";
+			modal.style.display = "block";
+		}
+	});
 
-    if (useAI) {
-        aiPlayer();
-    }
+	updateActivePlayer();
 
-    if (activePlayer == player1) {
-        activePlayer = player2;
-        player2Card.style.border = "5px solid red";
-        player1Card.style.border = "1px solid black";
-    } else {
-        activePlayer = player1;
-        player1Card.style.border = "5px solid red";
-        player2Card.style.border = "1px solid black";
-    }
-}
+	if (useAI) {
+		aiPlayer();
+		updateActivePlayer();
+	}
+};
+
+const updateActivePlayer = () => {
+	if (activePlayer == player1) {
+		activePlayer = player2;
+		player2Card.style.border = "5px solid red";
+		player1Card.style.border = "1px solid black";
+	} else {
+		activePlayer = player1;
+		player1Card.style.border = "5px solid red";
+		player2Card.style.border = "1px solid black";
+	}
+};
 
 const addClickHandler = (elem) => {
-    elem.addEventListener("click", () => {
-        if (elem.innerText == "") {
-            elem.innerText = activePlayer.marker;
-            gameBoard.gameArr[elem.id] = activePlayer.marker;
-            gameHandler();
-        }
-    });
-}
+  elem.addEventListener("click", () => {
+    if (elem.innerText == "" && !gameOver) {
+      elem.innerText = activePlayer.marker;
+      gameBoard.gameArr[elem.id] = activePlayer.marker;
+      gameHandler();
+    }
+  });
+};
 
 const resetGame = () => {
-    modal.style.display = "none";
-    const cells = Array.from(document.getElementsByTagName("td"));
-    cells.forEach(element => {
-        element.innerText = "";
-    });
-    gameBoard.gameArr = new Array(9);
-}
+	gameOver = false;
+	modal.style.display = "none";
+	const cells = Array.from(document.getElementsByTagName("td"));
+	cells.forEach((element) => {
+		element.innerText = "";
+	});
+	gameBoard.gameArr = new Array(9);
+};
 
-const drawResetButton = (parentElement) => {
-    const resetButton = document.createElement("button");
-    resetButton.innerText = "Reset";
-    resetButton.classList.add("button", "reset");
-    resetButton.addEventListener("click", () => {
-        resetGame();
-    });
-    parentElement.appendChild(resetButton);
-}
+const drawResetButton = (parentElement, text) => {
+	const resetButton = document.createElement("button");
+	resetButton.innerText = text;
+	resetButton.classList.add("button", "reset");
+	resetButton.addEventListener("click", () => {
+		resetGame();
+	});
+	parentElement.appendChild(resetButton);
+};
 
 const drawBoard = (elemID) => {
-    const gameElem = document.getElementById(elemID);
-    const table = document.createElement("table");
-    table.classList.add("centered", "noselect");
-    drawResetButton(gameElem);
-    gameElem.appendChild(table);
-    
-    for (let i = 0; i < gameBoard.gameArr.length / 3; i++) {
-        const tr = document.createElement("tr");
-        table.appendChild(tr);
-        for (let j = 0; j < gameBoard.gameArr.length / 3; j++) {
-            const td = document.createElement("td");
-            tr.appendChild(td);
-            addClickHandler(td);
-        }
-    }
+	const gameElem = document.getElementById(elemID);
+	const table = document.createElement("table");
+	table.classList.add("centered", "noselect");
+	drawResetButton(gameElem, "Reset");
+	gameElem.appendChild(table);
 
-    const cells = document.getElementsByTagName("td");
-    for (let i = 0; i < cells.length; i++) {
-        const cell = cells[i];
-        cell.id = i;
-    }
+	for (let i = 0; i < gameBoard.gameArr.length / 3; i++) {
+		const tr = document.createElement("tr");
+		table.appendChild(tr);
+		for (let j = 0; j < gameBoard.gameArr.length / 3; j++) {
+			const td = document.createElement("td");
+			tr.appendChild(td);
+			addClickHandler(td);
+		}
+	}
 
-    const playerList = Array.from((document.getElementsByClassName("player")));
-    playerList.forEach(element => {
-        const nameButton = document.createElement("button");
-        nameButton.classList.add("button", "btnSmall");
-        nameButton.innerText = "Change name";
-        nameButton.addEventListener("click", () => {
-            console.log("Change player name");
-        });
-        // element.appendChild(nameButton);
-    });
-    
+	const cells = document.getElementsByTagName("td");
+	for (let i = 0; i < cells.length; i++) {
+		const cell = cells[i];
+		cell.id = i;
+	}
+
+	const modalContent = document.getElementById("modalContent");
+	drawResetButton(modalContent, "Play Again");
+	const closeModal = Array.from(document.getElementsByClassName("closeModal"));
+	closeModal.forEach((element) => {
+		element.addEventListener("click", (event) => {
+			event.target.parentElement.style.display = "none";
+			closeModals();
+		});
+	});
+};
+
+const minMaxMax = () => {
+
+}
+
+const minMaxMin = () => {
+	
 }
 
 const aiPlayer = () => {
-    if (useAI) {
-        console.log("WE ARE THE ROBOTS");
-    }
+	const difficulty = "Easy";
+
+	// TO DO: Highlight AI decision process
+	let cells = Array.from(document.getElementsByTagName("td"));
+	let emptyCells = [];
+	
+	if (difficulty == "Easy") {
+		
+		
+		for (let i = 0; i < cells.length; i++) {
+			if (cells[i].innerText == "") {
+				emptyCells.push(i);
+			}
+		}
+		console.log(emptyCells);
+		let targetCell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+		cells[targetCell].innerText = activePlayer.marker;
+	}
+
+	if (difficulty == "Impossible") {
+		console.log("I don't know what to do here");
+	}
+};
+
+const closeModals = () => {
+	modals.forEach((element) => {
+		element.style.display = "none";
+	});
+	if (gameOver) {
+		resetGame();
+	}
 };
 
 const winnerName = document.getElementById("winnerName");
-const modal = document.getElementById("modal");
-const modalContent = document.getElementById("modalContent");
-const closeModal = document.getElementById("closeModal");
-closeModal.onclick = () => {
-    modal.style.display = "none";
-    resetGame();
-};
-
-drawResetButton(modalContent);
-
-const useAI = true;
-
-window.onload = drawBoard(gameBoard.gameElem);
+const modals = Array.from(document.getElementsByClassName("modal"));
+modals.forEach((element) => {
+	element.addEventListener("click", (event) => {
+		if (event.target.classList[0] == "modal") {
+			closeModals();
+		}
+	});
+});
 
 let activePlayer;
-const player1 = player("Player 1", "X");
-const player2 = player("Player 2", "O");
-gameHandler();
-updateScores();
+let gameOver = false;
+const useAI = true;
+
+const player1 = player("Player 1", "X", 0);
+const player2 = player("Player 2", "O", 1);
+
+const player1Card = document.getElementById("player1");
+const player2Card = document.getElementById("player2");
+const player1NameChange = document.getElementById("player1NameSelect");
+player1NameChange.addEventListener("click", () => {
+	let newName = window.prompt("Enter your name:");
+	if (newName != "") {
+		player1.name = newName;
+	}
+	updateScores();
+});
+const player2NameChange = document.getElementById("player2NameSelect");
+player2NameChange.addEventListener("click", () => {
+	let newName = window.prompt("Enter your name:");
+	if (newName != "") {
+		player2.name = newName;
+	}
+	updateScores();
+})
+
+window.onload = () => {
+	activePlayer = player2;
+	if (useAI) {
+		activePlayer.name = "Mr Robot";
+	}
+	resetGame();
+	updateActivePlayer();
+	updateScores();
+	drawBoard(gameBoard.gameElem);
+};
